468b823ed7c8766a4f1c0e5535a91bc81e9bd8a6
ahmad <ahmedasaad71421@gmail.com>
2026-04-05 17:04:31 +0200

    cookies athentication register login

---
+++ StoreWebapi.Application/Features/User/log/LogIn/LoginUserHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using StoreWebapi.Application.Common;
+using StoreWebapi.Domain.Domain;
+
+namespace StoreWebapi.Application.Features.User;
+
+public class LoginUserHandler : IRequestHandler<LoginUserCommand,Result<LoginUserResponse>>
+{
+    private readonly SignInManager<user> _signInManager;
+    private readonly UserManager<user> _userManager;
+    public LoginUserHandler(SignInManager<user> signInManager, UserManager<user> userManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+    public async Task<Result<LoginUserResponse>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+        if (user == null)
+            return Result<LoginUserResponse>.Failure<LoginUserResponse>("Invalid email or password.");
+
+      
+        var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+            var response = new LoginUserResponse
+            {
+                UserId = user.Id.ToString(),
+                Email = user.Email,
+                UserName = user.UserName
+            };
+            return Result<LoginUserResponse>.Success(response);
+        }
+        
+        return Result<LoginUserResponse>.Failure<LoginUserResponse>("user not allowed");
+    }
+}
